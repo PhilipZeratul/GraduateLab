e76b3b1ec99382bce205540df63ae38d87c7b072
Phillip Zeratul <p.f.zeratul@gmail.com>
2017-04-23 00:11:56 +0800

    player can not move

--- Assets/Scripts/Monobehaviours/Player/PlayerMovement.cs
+++ Assets/Scripts/Monobehaviours/Player/PlayerMovement.cs
@@ -1,34 +1,48 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
 
 
 public class PlayerMovement : MonoBehaviour
 {
-	public float horizantalSpeed = 10.0f;
-	public float verticalSpeed = 6.0f;
+	public float speed = 0.12f;
+    public const float inputHoldDelay = 0.5f;
 
-	private bool isWalking = false;
+	private bool isMouseClickedWalking = false;
+    private bool handleInput = true;
 	private Animator walkAnimator;
+    private Interactable currentInteractable = null;
+    private Vector3 destinationPosition;
+    private WaitForSeconds inputHoldWait;
+
+    private readonly int hashSpeedPara = Animator.StringToHash("Speed");   
+    private readonly int hashLocomotionTag = Animator.StringToHash("Locomotion");
 
 
-	void Awake()
+    private void Awake()
 	{
 		walkAnimator = GetComponent<Animator>();
+        inputHoldWait = new WaitForSeconds(inputHoldDelay);
 	}
 
-	void LateUpdate()
+	private void LateUpdate()
 	{
 		float h = Input.GetAxisRaw("Horizontal");
 		float v = Input.GetAxisRaw("Vertical");
 
 		if (h != 0 || v != 0)
-			Move(h, v);
-		else
+			KeyBoardMove(h, v);
+        else if (isMouseClickedWalking)
+            MouseMove();
+        else
 			Stop();
-	}
+    }
 
-	void Move(float h, float v)
+	private void KeyBoardMove(float h, float v)
 	{
+        isMouseClickedWalking = false;
+        currentInteractable = null;
+
         if (h > 0)
         {
             transform.localRotation = Quaternion.Euler(0, 180, 0);
@@ -39,12 +53,83 @@ public class PlayerMovement : MonoBehaviour
             transform.localRotation = Quaternion.Euler(0, 0, 0);
         }
 
-        transform.Translate(h*horizantalSpeed, v*verticalSpeed, 0.0f);
-		walkAnimator.SetFloat("Speed", 2.0f);
+        transform.Translate(h*speed, v*speed, 0.0f);
+		walkAnimator.SetFloat(hashSpeedPara, 2.0f);
 	}
 
-	void Stop()
+    private void MouseMove()
+    {        
+       /* if (Vector2.Distance(transform.position, destinationPosition) < 0.2f)
+        {
+            isMouseClickedWalking = false;            
+            return;
+        }
+        if (destinationPosition.x > transform.position.x)
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+        else if (destinationPosition.x < transform.position.x)
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+
+        transform.position = Vector3.MoveTowards(transform.position, destinationPosition, speed);
+        walkAnimator.SetFloat(hashSpeedPara, 2.0f);*/
+    }
+
+	private void Stop()
 	{
-		walkAnimator.SetFloat("Speed", 0.0f);
-	}
+        isMouseClickedWalking = false;
+		walkAnimator.SetFloat(hashSpeedPara, 0.0f);
+
+       /* if (currentInteractable)
+        {
+            // ... set the player's rotation to match the interactionLocation's.
+            transform.rotation = currentInteractable.interactionLocation.rotation;
+
+            // Interact with the interactable and then null it out so this interaction only happens once.
+            currentInteractable.Interact();
+            currentInteractable = null;
+
+            // Start the WaitForInteraction coroutine so that input is ignored briefly.
+            StartCoroutine(WaitForInteraction());
+        }*/
+    }
+
+    public void OnGroundClick(BaseEventData data)
+    {
+        if (!handleInput)
+            return;
+        currentInteractable = null;
+
+        PointerEventData pData = (PointerEventData)data;
+        destinationPosition = pData.pointerCurrentRaycast.worldPosition;
+        isMouseClickedWalking = true;
+        walkAnimator.SetFloat(hashSpeedPara, 0.0f);
+    }
+
+    public void OnInteractableClick(Interactable interactable)
+    {
+        if (!handleInput)
+            return;
+        currentInteractable = interactable;
+
+        destinationPosition = interactable.interactionLocation.position;
+        isMouseClickedWalking = true;
+        walkAnimator.SetFloat(hashSpeedPara, 0.0f);
+    }
+
+    private IEnumerator WaitForInteraction()
+    {
+        // As soon as the wait starts, input should no longer be accepted.
+        handleInput = false;
+
+        // Wait for the normal pause on interaction.
+        yield return inputHoldWait;
+
+        // Until the animator is in a state with the Locomotion tag, wait.
+        while (walkAnimator.GetCurrentAnimatorStateInfo(0).tagHash != hashLocomotionTag)
+        {
+            yield return null;
+        }
+
+        // Now input can be accepted again.
+        handleInput = true;
+    }
 }
